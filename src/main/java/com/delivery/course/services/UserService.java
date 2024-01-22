d439b0b6c3bd11c42a458b019e07b3531764c5e9
Bruno Costa <magalhaes.brunno1@gmail.com>
2024-01-22 11:05:26 -0300

    exception handling- update

--- src/main/java/com/delivery/course/services/UserService.java
+++ src/main/java/com/delivery/course/services/UserService.java
@@ -26,7 +26,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 
-		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
@@ -47,16 +47,24 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		
-		updateData(entity, obj);
-		
-		return repository.save(entity);
-				
+
+		try {
+			User entity = repository.getReferenceById(id);
+
+			updateData(entity, obj);
+
+			return repository.save(entity);
+
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			
+			throw new ResourceNotFoundException(id);
+		}
+
 	}
 
 	private void updateData(User entity, User obj) {
-	
+
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
